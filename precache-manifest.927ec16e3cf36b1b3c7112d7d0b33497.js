1b178213c81b9c5d0309cdeade0e356c8888256e
vitaliyis <vetal.friend@gmail.com>
2020-10-21 13:19:43 +0300

    Updates

---
+++ precache-manifest.927ec16e3cf36b1b3c7112d7d0b33497.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d5e4b23f8ba0510325941891a3b63c",
+    "url": "/random-user/index.html"
+  },
+  {
+    "revision": "9674446aa5e42f27acab",
+    "url": "/random-user/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "124dbff4c16f2375597d",
+    "url": "/random-user/static/css/main.28ae0eba.chunk.css"
+  },
+  {
+    "revision": "9674446aa5e42f27acab",
+    "url": "/random-user/static/js/2.edff8f2a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/random-user/static/js/2.edff8f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124dbff4c16f2375597d",
+    "url": "/random-user/static/js/main.2d311a51.chunk.js"
+  },
+  {
+    "revision": "2493799319cf64824d37",
+    "url": "/random-user/static/js/runtime-main.39e6d62a.js"
+  },
+  {
+    "revision": "fcfaaffa1c39b3ce751bba445533607f",
+    "url": "/random-user/static/media/vetal.fcfaaffa.jpg"
+  }
+]);
